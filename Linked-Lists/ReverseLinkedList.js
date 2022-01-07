2371c150b5e59a4d1ff3b798bd41b6205c389503
Anshu Patel <anshupatel314@gmail.com>
2022-01-07 15:09:20 -0500

    iterative solution for reversing linked list

---
+++ Linked-Lists/ReverseLinkedList.js
@@ -0,0 +1,15 @@
+// LeetCode Problem #206
+
+/*
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+Example:
+    Input: [1,2,3,4,5]
+    Output: [5,4,3,2,1]
+*/
+
+function ListNode(val, next) {
+  this.val = val === undefined ? 0 : val;
+  this.next = next === undefined ? null : next;
+}
+
+function reverseList(head) {}
