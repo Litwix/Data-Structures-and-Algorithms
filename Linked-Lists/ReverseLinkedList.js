030e1f9af5faf4c2ead51ae3852952f8bbf2c7cf
Anshu Patel <anshupatel314@gmail.com>
2022-01-07 15:22:22 -0500

    added recursive solution to reverse linked list

--- Linked-Lists/ReverseLinkedList.js
+++ Linked-Lists/ReverseLinkedList.js
@@ -12,15 +12,36 @@ function ListNode(val, next) {
   this.next = next === undefined ? null : next;
 }
 
-function reverseList(head) {
+// ITERATIVE
+function reverseList1(head) {
   let newHead = null;
 
+  // Essentially having 3 pointers in following order: newHead, head, nextNode
+  // Once head becomes null, newHead is pointing to the last node in the original linked list
+  // and every direction has been reversed
   while (head) {
     let nextNode = head.next;
-    head.next = newHead;
-    newHead = head;
+    head.next = newHead; // reversing the direction here
+    newHead = head; // then increment pointers
     head = nextNode;
   }
 
   return newHead;
+}
+
+// RECURSIVE
+function reverseList2(head) {
+  return reverseListRecursive(head, null);
+}
+
+// Similar logic here where pointers are in same order
+// However, here we terminate when nextNode is null (rather than head) and return the head
+function reverseListRecursive(head, newHead) {
+  if (head === null) {
+    return newHead;
+  } else {
+    let nextNode = head.next;
+    head.next = newHead; // reversing direction here
+    return reverseListRecursive(nextNode, head); // incrementing here by putting in new positions for the pointers
+  }
 }
